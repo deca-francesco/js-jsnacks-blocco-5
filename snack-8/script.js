fa51d8bfe0b6cdcdcc4c2ee80a14790950bf8b25
deca-francesco <francescodecarlo70@gmail.com>
2024-10-16 17:07:06 +0200

    ADD: snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,25 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+// find implicito
+const marco = students.find(student => student.name === "Marco Lanci");
+console.log(`La classe di Marco è la: ${marco.class}`);
+
+
+
+// find esplicito
+const luigi = students.find(student => {
+  if (student.name === "Luigi Banzi") {
+    console.log(`La classe di Luigi è la: ${student.class}`);
+  }
+});
+
+
+// ciclo for
+for (i = 0; i < students.length; i++) {
+  const student = students[i];
+  if (student.name === "Mario Banfi") {
+    console.log(`La classe di Mario è la: ${student.class}`);
+  }
+}
