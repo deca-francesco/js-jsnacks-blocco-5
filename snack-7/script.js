a48adb234f7e6b8f6780f44c9cb26ec9ca11c6d1
deca-francesco <francescodecarlo70@gmail.com>
2024-10-16 16:54:53 +0200

    ADD: snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,30 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+
+// find implicito
+const studente = students.find(student => student.id === 2);
+console.log(studente);
+
+
+
+// find esplicito
+const studente2 = students.find(student => {
+  if (student.id === 2) {
+    return true
+  }
+});
+console.log(studente2);
+
+
+
+// ciclo for
+let studenteFor = {};
+for (i = 0; i < students.length; i++) {
+  const studente3 = students[i];
+  if (studente3.id === 3) {
+    studenteFor = studente3;
+  }
+}
+console.log(studenteFor);
