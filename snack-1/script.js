cef3e291ae01547df95a95696371f0b1641d7201
deca-francesco <francescodecarlo70@gmail.com>
2024-10-16 15:39:23 +0200

    initial commit + ADD: snack-1

---
+++ snack-1/script.js
@@ -0,0 +1,13 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+// log dell'array
+console.log(names);
+
+// log dell'elemento ad ogni iterazione
+names.forEach(name => console.log(name));
+
+// log dell'array trasformato in stringa
+console.log(names.join());
