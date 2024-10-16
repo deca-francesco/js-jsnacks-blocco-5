1f664b6428e56d10083bc1ff0bbbfece002cd7e4
deca-francesco <francescodecarlo70@gmail.com>
2024-10-16 16:02:10 +0200

    ADD: snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,16 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+// map
+const authors = posts.map(post => post.author);
+console.log(authors);
+
+
+// ciclo for
+const authorsFor = [];
+for (let i = 0; i < posts.length; i++) {
+  const author = posts[i].author;
+  authorsFor.push(author);
+}
+console.log(authorsFor);
